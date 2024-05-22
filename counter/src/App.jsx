e9d5583bdccb0b9919fd2122c1c48b01850a4915
shruti248 <shrutiunadkat5@gmail.com>
2024-05-22 12:44:55 +0530

    Rules for conditional rendering

--- counter/src/App.jsx
+++ counter/src/App.jsx
@@ -77,6 +77,13 @@ function App() {
     }
   }
 
+  // In React, you control branching logic with JavaScript.
+// You can return a JSX expression conditionally with an if statement.
+// You can conditionally save some JSX to a variable and then include it inside other JSX by using the curly braces.
+// In JSX, {cond ? <A /> : <B />} means “if cond, render <A />, otherwise <B />”.
+// In JSX, {cond && <A />} means “if cond, render <A />, otherwise nothing”.
+// The shortcuts are common, but you don’t have to use them if you prefer plain if.
+
   return (
     <>
       <h1>Counter App</h1>
