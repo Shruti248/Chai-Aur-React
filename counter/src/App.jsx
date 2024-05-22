42985af777c12a939af8cbae6eed72a6664aad0b
shruti248 <shrutiunadkat5@gmail.com>
2024-05-22 12:40:38 +0530

    Conditional Rendering

--- counter/src/App.jsx
+++ counter/src/App.jsx
@@ -21,39 +21,82 @@ function App() {
 
 
 
-   // Eeach hooks -- perfoms specific task only 
+  // Eeach hooks -- perfoms specific task only 
 
   //  useState  --- resposnible for chnaging teh state of teh applicatin --- thsi state is propogated in the UI -- in the DOM -- so changes rae visible accoridng to teh cgange in the state 
 
-  let [/** value */ counter , setCounter /** Function */] = useState(/**Default value */ 0);
+  let [/** value */ counter, setCounter /** Function */] = useState(/**Default value */ 0);
   // setCounter controls teh variable counter ---- 
   // then when react knows taht the counter is update d-- it automatically updates the \varibale wherever it is used ----- all teh places automaticallyyyyy 
 
 
   const addValue = () => {
     console.log("Add value Clicked...")
-    // counter= counter+1;
-    setCounter(++counter);
+
+    if (counter == 20) {
+      setErrMsg('Counter should not exceed 20');
+    } else {
+      // counter= counter+1;
+      setCounter(++counter)
+      setErrMsg('');
+    }
+
+
   }
   const removeValue = () => {
     console.log("Remove value Clicked...")
-    // counter= counter-1;
-    setCounter(--counter);
+
+    if (counter == 0) {
+      setErrMsg('Counter cannot be decreased further...');
+    } else {
+
+      // counter= counter-1;
+      setCounter(--counter);
+      // Clear the error message if within the valid range 
+      setErrMsg('');
+    }
+
   }
 
   // Now teh UI & Satte are synced --- whenever state changes -- ui automatically updates
 
   // This is how teh cahges are propagated in the REACt APP
 
+  // Task -- value shoudl be between  0 to 20 only 
+
+  let [errMsg, setErrMsg] = useState('');
+
+  // --------- Thsi methods are for RENDERING BASED ON CONDITIONS ONLY ---- 
+  
+  // Method 2: Helper Function for Conditional Rendering
+  let renderErrMsg = () => {
+    if (errMsg != '') {
+      return <p>{errMsg}</p>
+    } else {
+      return <p></p>
+    }
+  }
+
   return (
     <>
-        <h1>Counter App</h1>
-        <h2>Counter : {counter}</h2>
+      <h1>Counter App</h1>
+      <h2>Counter : {counter}</h2>
 
 
-        <button onClick={addValue}>Add Value</button>
-        <br />
-        <button onClick={removeValue}>Remove Value</button>
+      <button onClick={addValue}>Add Value</button>
+      <br />
+      <button onClick={removeValue}>Remove Value</button>
+      <div>
+
+      {/* {errMsg && <p>{errMsg}</p>} -----  conditionally rendering components based on a boolean value.  */}
+      {/* id errMsg is true -- it evalutes teh right hand side expression  */}
+      {/* if errMsg is false --- it retuns & does not evalute the right hand side expression  */}
+
+
+      {/* // Method 1: Inline Conditional Rendering with Ternary Operator  */}
+      {errMsg && <p>{errMsg}</p>}
+        {/* {renderErrMsg()} */}
+      </div>
     </>
   )
 }
